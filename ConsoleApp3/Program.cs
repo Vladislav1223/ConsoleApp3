fa8581375c4fcd27f67e5748683d012e1dfadea6
Vladislav <vlad.king.20012109@gmail.com>
2021-04-08 09:11:01 +0300

    zmina 4

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -14,6 +14,11 @@ namespace ConsoleApp3
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
